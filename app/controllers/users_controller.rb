2ab7241664f72aeeb3a9a4bbbbfab274e9d44def
williamkennedy <williamkennedyjnr@gmail.com>
2015-03-25 15:49:19 +0000

    building an edit function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,24 @@ class UsersController < ApplicationController
     else
       render :new
     end
+    
+    def edit
+      @user = User.find(params[:id])
+    end
+    
+    def update
+      @user = User.find(params[:id])
+      if 
+        @user.update(user_params)
+      # redirect_to edit_user_path(@user)
+        redirect_to users_path
+      else
+        render :edit
+      end
+end
+    
   end
+  
   
   private 
   
